6fc14ac1b91abd523dae33a27bd8aae1668f1ccd
Thomas Degenetais <thomas-degenetais@outlook.fr>
2020-06-13 19:21:34 +0200

    shutdown computer

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -4,62 +4,77 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Formatter;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 /**
  * 
- * @author Thomas DEGENETAIS 
+ * @author Deegmas
  * @version 1.0 2017-04-07
- * Programme permettant d'eteindre l'ordinateur à une heure donné 
+ * Programme permettant d'eteindre l'ordinateur à une heure définie 
  */
 public class Fenetre extends JFrame implements ActionListener
 {
 	
-	JButton btnValider;
-	JButton btnEffacer;
-	JTextField txt;
+	JButton 	btnValider;
+	JComboBox 	listeHeure;
+	JComboBox 	listeMinute;
+	JLabel		labelHeure;
+	JLabel		labelMinute;
+	
+	Object heures[]  = new Object[] {"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
+	
+	Object minutes[] = new Object[] {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", 
+									"20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", 
+									"40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"};
 	
 	public Fenetre()
 	{
 		this.setTitle("Eteindre l'ordinateur");
-		this.setSize(600,600);
+		this.setSize(400,200);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		JPanel pan = new JPanel();
-		pan.setLayout(new GridLayout(3, 1));
-		txt = new JTextField();
-		btnValider = new JButton("Valider");
-		btnEffacer = new JButton("Effacer");
-		pan.add(txt);
+		
+		JPanel pan 	= new JPanel();
+		pan.setLayout(new GridLayout(5, 1));
+		labelHeure 	= new JLabel("Choisissez une heure :");
+		listeHeure 	= new JComboBox(heures);
+		labelMinute = new JLabel("Choisissez les minutes");
+		listeMinute = new JComboBox(minutes);
+		btnValider 	= new JButton("Valider");
+		pan.add(labelHeure);
+		pan.add(listeHeure);
+		pan.add(labelMinute);
+		pan.add(listeMinute);
 		pan.add(btnValider);
-		pan.add(btnEffacer);
 		
 		btnValider.addActionListener(this);
-		btnEffacer.addActionListener(this);
 		
 		add(pan);
 		
 		this.setVisible(true);
 		
 		
-		while(true)
+		/*while(true)
 		{
-			gestionDuTemps(); 
-		}
+			gestionDuTemps();
+		}*/
 	}
 	
 	/**
 	 *  methode pour lancer la commande 
 	 * @return le nom de la commande a executer
 	 */
-	private String getCommand() {
-		return "shutdown /s";
+	private String getCommande() {
+		return "shutdown -f -s";
 	}
 	
 	/*
@@ -85,65 +100,49 @@ public class Fenetre extends JFrame implements ActionListener
 			
 			String s = formater.format(date);
 			
-			/*System.out.println(formater.format(date));
-			System.out.println("--------------------");
-			System.out.println(s);
-			System.out.println("--------------------");*/
-			
 			return s;
 		}
 	}
 
 	/**
-	 * Gestion des evenements au clics des boutons quitter et valider
+	 * Gestion des evenements au click du bouton valider
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
-		if (e.getSource() == btnEffacer)
-		{
-			txt.setText("");
-		}
 		if (e.getSource() == btnValider)
 		{
-			int heure = Integer.parseInt(txt.getText().toString().substring(0,2));
-			//System.out.println(heure);
-			int minutes = Integer.parseInt(txt.getText().toString().substring(2,4));
-			//System.out.println(minutes);
+			int heure = Integer.parseInt(listeHeure.getSelectedItem().toString());
+			int minute = Integer.parseInt(listeMinute.getSelectedItem().toString());
 			
 			
 			boolean vrai = true;
 			while(vrai) 
 			{
 				String temps = gestionDuTemps().toString();
-				//System.out.println(temps);
 				
 				int h = Integer.parseInt(temps.toString().substring(0, 2));
 				int m = Integer.parseInt(temps.toString().substring(3,5));
-				//System.out.println(h + " minutes " + m);
 				
-				if (heure == h && minutes == m)
+				if (heure == h && minute == m)
 				{
 					
 					System.out.println("Arret de la machine en cours");
 					vrai = false;
-					//System.exit(0);
 
 					Runtime r = Runtime.getRuntime();
 					try 
 					{
-						r.exec(getCommand());
+						r.exec(getCommande());
 					} catch (IOException ioe) {
 						ioe.printStackTrace();
 					}
 				}
-			}			
+			}		
 		}
 	}
 	
-	/**
-	 * Main du programme 
-	 */
+
 	public static void main(String[] args) 
 	{
 		new Fenetre();
